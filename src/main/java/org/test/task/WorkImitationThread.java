97ecae3dafad084fe1ba6920d5a802caefa0d1c5
= <=>
2025-08-07 02:04:16 +0300

    add comments

--- src/main/java/org/test/task/WorkImitationThread.java
+++ src/main/java/org/test/task/WorkImitationThread.java
@@ -21,29 +21,39 @@ public class WorkImitationThread implements Runnable {
     @Override
     public void run() {
         while (true) {
+            // Начало итерации. Будем измерять общее время выполнения, чтобы соблюсти период
             System.out.println(Thread.currentThread().getName() + " started work");
 
             long start = System.currentTimeMillis();
             try {
+                // Получаем случайные индексы двух разных локов из общего списка
                 int[] twoRandomLockIndex = getTwoRandomLockIndex(locks.size());
                 System.out.println(Thread.currentThread().getName() + " take locks with index " + Arrays.toString(twoRandomLockIndex));
 
                 Lock lock1 = locks.get(twoRandomLockIndex[0]);
                 Lock lock2 = locks.get(twoRandomLockIndex[1]);
 
+                /*
+                    Пытаемся захватить оба лока
+                    Если один из локов занят другим потоком — освобождаем занятый (если есть)
+                 */
                 if (tryLock(lock1, lock2)) {
+                    // Имитируем работу с двумя блокирующими ресурсами
                     long sleepTime = getRandomSleepTime(threadInfo.getIntervalFrom(), threadInfo.getIntervalTo());
                     System.out.println(Thread.currentThread().getName() + " will sleep for " + sleepTime / 1000 + " sec");
                     Thread.sleep(sleepTime);
 
+                    // Освобождаем оба лока
                     unlock(lock1, lock2);
                 } else {
                     System.out.println(Thread.currentThread().getName() + " didn't take objects lock: one of they is busy");
                 }
 
+                // Рассчитываем задержку до следующей итерации, чтобы сохранить общее время = период
                 long startDelay = getStartDelay(start);
                 System.out.println(Thread.currentThread().getName() + " will wait " + startDelay / 1000d + " for next iteration");
 
+                // Пауза до следующей итерации, если осталось время
                 Thread.sleep(startDelay);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
@@ -56,6 +66,11 @@ public class WorkImitationThread implements Runnable {
         return millisLeft > 0 ? millisLeft : 0;
     }
 
+    /**
+     * Пытаемся захватить оба лока без блокировки.
+     * Если второй не удаётся — освобождаем первый.
+     * Это гарантирует отсутствие дедлока
+     */
     private boolean tryLock(Lock lock1, Lock lock2) {
         if (lock1.tryLock()) {
             if (lock2.tryLock()) {
--- src/main/java/org/test/task/utils/RandomUtil.java
+++ src/main/java/org/test/task/utils/RandomUtil.java
@@ -5,10 +5,16 @@ import java.util.Random;
 public class RandomUtil {
     private static final Random random = new Random();
 
+    /**
+     * Возвращает случайное значение задержки в миллисекундах из заданного диапазона
+     */
     public static long getRandomSleepTime(int from, int to) {
         return random.nextInt(from, to + 1) * 1000L;
     }
 
+    /**
+     * Возвращает два различных случайных индекса из диапазона [0, locksCount)
+     */
     public static int[] getTwoRandomLockIndex(int locksCount) {
         int firstIdx = random.nextInt(locksCount);
         int secondIdx = random.nextInt(locksCount);
